a8b5f8fdafeeabda3f5d271c5dc62aa21b5810fe
Rodolphe <dutchr@live.fr>
2024-12-10 12:13:50 +0100

    :art: changer semantic UI pour Fomantic UI

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Projet;
+use App\Form\ProjetType;
 use App\Repository\ProjetRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +20,25 @@ class ProjetController extends AbstractController
     {
         return $this->render('projet/index.html.twig', [
             'projets' => $projetRepository->findAll(),
+        ]);
+    }
+    #[Route('/new', name: 'projet_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $projet = new Projet();
+        $form = $this->createForm(ProjetType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($projet);
+            $em->flush();
+
+            return $this->redirectToRoute('projet_index');
+        }
+
+        return $this->render('projet/new.html.twig', [
+            'projet' => $projet,
+            'form' => $form->createView(),
         ]);
     }
 }
