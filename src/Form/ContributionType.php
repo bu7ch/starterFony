5cea1745718958738846bb229ec0a1f7e1f51e51
Rodolphe <dutchr@live.fr>
2024-12-10 16:56:45 +0100

    :sparkles: WIP need to fix date for contribution

--- src/Form/ContributionType.php
+++ src/Form/ContributionType.php
@@ -2,38 +2,41 @@
 
 namespace App\Form;
 
-use App\Entity\Contribution;
 use App\Entity\Projet;
 use App\Entity\Utilisateur;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Contribution;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class ContributionType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('montant')
-            ->add('date', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('utilisateur', EntityType::class, [
-                'class' => Utilisateur::class,
-                'choice_label' => 'email',
-            ])
-            ->add('projet', EntityType::class, [
-                'class' => Projet::class,
-                'choice_label' => 'titre',
-            ])
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('montant')
+      ->add('date', DateTimeType::class, [
+        'widget' => 'single_text',
+        'html5' => false,
+        'attr' => ['class' => 'ui calendar date-input'],
+      ])
+      ->add('utilisateur', EntityType::class, [
+        'class' => Utilisateur::class,
+        'choice_label' => 'email',
+      ])
+      ->add('projet', EntityType::class, [
+        'class' => Projet::class,
+        'choice_label' => 'titre',
+      ])
+    ;
+  }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Contribution::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Contribution::class,
+    ]);
+  }
 }
