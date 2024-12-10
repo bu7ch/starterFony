01a1dc57d5851483a37ea82bfa3cae25185579f0
Rodolphe <dutchr@live.fr>
2024-12-10 11:35:51 +0100

    🚧 Ajout des controlleurs(utilisateur/projet/contribution)

---
+++ src/Controller/ProjetController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/projets')]
+class ProjetController extends AbstractController
+{
+    #[Route('/', name: 'projet_index')]
+    public function index(): Response
+    {
+        return $this->render('projet/index.html.twig', [
+            'controller_name' => 'ProjetController',
+        ]);
+    }
+}
---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/utilisateur')]
+class UtilisateurController extends AbstractController
+{
+    #[Route('/', name: 'utilisateur_index')]
+    public function index(): Response
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'controller_name' => 'UtilisateurController',
+        ]);
+    }
+}
