96456fd3133185dfcc661d33a1b138069c5f47ec
Rodolphe <dutchr@live.fr>
2024-12-10 11:41:48 +0100

    :sparkles: Ajout de la methode index

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProjetRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 #[Route('/projets')]
 class ProjetController extends AbstractController
 {
     #[Route('/', name: 'projet_index')]
-    public function index(): Response
+    public function index(ProjetRepository $projetRepository): Response
     {
         return $this->render('projet/index.html.twig', [
-            'controller_name' => 'ProjetController',
+            'projets' => $projetRepository->findAll(),
         ]);
     }
 }
